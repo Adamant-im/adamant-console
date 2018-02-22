26f3772342d3da9a7ab53c3f50d383fe5e53e011
Zyuhel <zyuhel@yandex.ru>
2018-02-22 12:42:37 +0300

    add initial vorpal handler

---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const admConsole = require('vorpal')();
+
+const opsDir = path.join(__dirname, 'ops');
+
+fs.readdirSync(opsDir).forEach((operation) => {
+    const operationPath = path.join(opsDir, operation);
+    const operationComponent = require(operationPath);
+    admConsole.use(operationComponent);
+});
+
+admConsole.find('help').alias('?');
+admConsole.find('exit').alias('q');
+
+if (process.argv.length > 2) {
+  admConsole
+  .parse(process.argv);
+
+}
+else {
+  admConsole
+  .delimiter('adm:')
+  .show()
+  .parse(process.argv);
+}
