4f2d78927e73a15236c69c703bb20d3ac350f1e1
Zyuhel <zyuhel@yandex.ru>
2018-02-23 23:52:35 +0300

    add file with constants

---
+++ helpers/constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+    epochTime: new Date(Date.UTC(2017, 8, 2, 17, 0, 0, 0)),
+    fees: {
+	send: 50000000,
+	vote: 1000000000,
+	secondsignature: 500000000,
+	delegate: 30000000000,
+	multisignature: 500000000,
+	dapp: 2500000000,
+        old_chat_message: 500000,
+	chat_message: 100000,
+	profile_update:  5000000,
+	avatar_upload: 10000000
+    },
+    maxVotesPerTransaction: 33
+}
