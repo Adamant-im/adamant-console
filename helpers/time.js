edb4e0592d4bed2c2a3301307ff469b6a798e243
Zyuhel <zyuhel@yandex.ru>
2018-03-10 20:28:53 +0300

    Use epoch from constants, in future it should be overriden by config for custom networks

--- helpers/time.js
+++ helpers/time.js
@@ -1,10 +1,11 @@
+const constants = require('./constants.js');
 module.exports = {
     getEpochTime: function (time) {
 	if (time === undefined) {
 	    time = Date.now();
 	}
 
-	var d = new Date(Date.UTC(2017, 8, 2, 17, 0, 0, 0));
+	var d = constants.epochTime;
 	var t = d.getTime();
 
 	return Math.floor((time - t) / 1000);
