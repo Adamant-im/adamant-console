33afc2371e7cac2e7075f17140f59d08780ad6c3
Zyuhel <zyuhel@yandex.ru>
2018-03-11 20:55:49 +0300

    adding vote for command

--- helpers/transactionFormer.js
+++ helpers/transactionFormer.js
@@ -4,60 +4,71 @@ var Mnemonic = require('bitcore-mnemonic')
 var bignum = require('./bignumber.js')
 var keys = require('./keys.js')
 var ByteBuffer = require('bytebuffer')
-const constants = require('./constants.js');
-const time = require('./time.js');
+const constants = require('./constants.js')
+const time = require('./time.js')
 module.exports = {
     createTransaction: function (type, data) {
-	switch (type) {
-	    case constants.transactionTypes.SEND:
-		break;
-	    case constants.transactionTypes.DELEGATE:
-		return this.createDelegateTransaction(data);
-		break;
-	    case constants.transactionTypes.CHAT_MESSAGE:
-		return this.createChatTransaction(data);
-		break;
-	}
-	return {};
+		switch (type) {
+			case constants.transactionTypes.SEND:
+				break;
+			case constants.transactionTypes.VOTE:
+				return this.createVoteTransaction(data)
+            case constants.transactionTypes.DELEGATE:
+                return this.createDelegateTransaction(data);
+			case constants.transactionTypes.CHAT_MESSAGE:
+				return this.createChatTransaction(data);
+		}
+		return {};
     },    
     createBasicTransaction: function (data) {
-//    	{,"asset":{"chat":{"message":"f691d8f74fa901ac30435dc762bcc12af4a45075","own_message":"66989fb35c5c1f9d269baef76521c0dfad8d5b35be514fad","type":1}},"recipientId":"U4463182680120420828","senderId":"U7972131227889954319","signature":"93d0456db3bb202aead3582b86a997a1a8e4bca429d1e6653b56d87025f4a2a5d9bb4202e854c0b927469d90872c59a526c3b798c62fd7b643ea3e03ce51380b"}
-	var transaction = {type: data.transactionType, amount: 0, timestamp: time.getTime(), asset: {}, senderPublicKey: data.keyPair.publicKey.toString('hex'), senderId: keys.createAddressFromPublicKey(data.keyPair.publicKey)};
-	return transaction;
+		var transaction = {type: data.transactionType, amount: 0, timestamp: time.getTime(), asset: {}, senderPublicKey: data.keyPair.publicKey.toString('hex'), senderId: keys.createAddressFromPublicKey(data.keyPair.publicKey)};
+		return transaction;
     },
     createChatTransaction: function (data) {
 	
     },
     createDelegateTransaction: function (data) {
-	data.transactionType = constants.transactionTypes.DELEGATE;
-	transaction = this.createBasicTransaction(data);
-	transaction.asset = {"delegate": { "username": data.username, publicKey: data.keyPair.publicKey.toString('hex')}};
-	transaction.recipientId= null;
-	transaction.signature = this.transactionSign(transaction, data.keyPair);
-	return transaction;
+		data.transactionType = constants.transactionTypes.DELEGATE
+		var transaction = this.createBasicTransaction(data)
+		transaction.asset = {"delegate": { "username": data.username, publicKey: data.keyPair.publicKey.toString('hex')}}
+		transaction.recipientId= null
+		transaction.signature = this.transactionSign(transaction, data.keyPair)
+		return transaction;
     },
+	createVoteTransaction: function (data) {
+        data.transactionType = constants.transactionTypes.VOTE
+        var transaction = this.createBasicTransaction(data)
+        transaction.asset = {"votes": data.votes}
+        transaction.recipientId= transaction.senderId
+        transaction.signature = this.transactionSign(transaction, data.keyPair)
+        return transaction
+	},
     getHash: function (trs) {
-	return crypto.createHash('sha256').update(this.getBytes(trs)).digest()
+		return crypto.createHash('sha256').update(this.getBytes(trs)).digest()
     },
     getBytes: function (transaction) {
-	var skipSignature = false
+		var skipSignature = false
         var skipSecondSignature = true
-	var assetSize = 0
-	var assetBytes = null
+		var assetSize = 0
+		var assetBytes = null
 
         switch (transaction.type) {
-	    case constants.transactionTypes.SEND:
-    	    break
-    	    case constants.transactionTypes.DELEGATE:
-    		assetBytes = this.delegatesGetBytes(transaction)
-    		assetSize = assetBytes.length;
-    	    break
-	    case constants.transactionTypes.CHAT_MESSAGE:
-    		assetBytes = this.chatGetBytes(transaction)
-    		assetSize = assetBytes.length
-    	    break
-	    default:
-    		 alert('Not supported yet')
+	    	case constants.transactionTypes.SEND:
+    	    	break
+			case constants.transactionTypes.DELEGATE:
+    				assetBytes = this.delegatesGetBytes(transaction)
+    				assetSize = assetBytes.length;
+    	    	break
+            case constants.transactionTypes.VOTE:
+                assetBytes = this.voteGetBytes(transaction)
+                assetSize = assetBytes.length;
+                break
+	    	case constants.transactionTypes.CHAT_MESSAGE:
+    			assetBytes = this.chatGetBytes(transaction)
+    			assetSize = assetBytes.length
+    	    	break
+	    	default:
+    			 alert('Not supported yet')
 	}
 
 	var bb = new ByteBuffer(1 + 4 + 32 + 8 + 8 + 64 + 64 + assetSize, true)
@@ -124,21 +135,30 @@ module.exports = {
 	return new Buffer(buffer)
     },
     transactionSign: function (trs, keypair) {
-	var hash = this.getHash(trs)
-	return this.sign(hash, keypair).toString('hex')
+		var hash = this.getHash(trs)
+		return this.sign(hash, keypair).toString('hex')
     },
+	voteGetBytes: function (trs) {
+        var buf
+        try {
+            buf = trs.asset.votes ? Buffer.from(trs.asset.votes.join(''), 'utf8') : null
+        } catch (e) {
+            throw e
+        }
+        return buf
+	},
     delegatesGetBytes: function (trs) {
         if (!trs.asset.delegate.username) {
-	    return null;
-	}
-	var buf;
+	    	return null;
+		}
+		var buf;
 
-	try {
-	    buf = Buffer.from(trs.asset.delegate.username, 'utf8');
-	} catch (e) {
-	    throw e;
-	}
-	return buf;
+		try {
+	    	buf = Buffer.from(trs.asset.delegate.username, 'utf8');
+		} catch (e) {
+	    	throw e;
+		}
+		return buf;
     },
     chatGetBytes: function (trs) {
 	var buf
---
+++ ops/vote_for.js
@@ -0,0 +1,30 @@
+/*
+
+*/
+const keys = require('../helpers/keys.js');
+const constants = require('../helpers/constants.js')
+const transactionFormer = require('../helpers/transactionFormer.js')
+const config = require('../helpers/configReader.js')
+const popsicle = require('popsicle')
+module.exports=function (vorpal) {
+    return vorpal.command('vote for <input...>').description('Add votes for delegates (or remove them by prepending - to delegate name').action(function(args, callback) {
+        var votes = args.input
+        for (var i in votes) {
+    	    if (votes[i][0]!=='-' && votes[i][0]!=='+')
+                votes[i]='+'+votes[i]
+        }
+		var keypair = keys.createKeypairFromPassPhrase(config.getConfig().passPhrase)
+    	var data = {type: constants.transactionTypes.VOTE, keyPair: keypair, votes: votes}
+    	var transaction = transactionFormer.createTransaction(data.type, data)
+        this.log(transaction)
+    	var self = this
+        popsicle.request({
+            method: 'POST',
+            url: config.getNodeConnectString()+'/api/accounts/delegates',
+            body: transaction
+        }).then(function (res) {
+            self.log(JSON.stringify(JSON.parse(res.body),null,4))
+            callback()
+        })
+    });
+};
