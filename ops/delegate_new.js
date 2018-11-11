c5980947b1b4cb5a1d20dca37325c77a7a7edba3
Zyuhel <zyuhel@yandex.ru>
2018-11-11 15:37:39 +0300

    Add support for --passPhrase option for delegate new command

--- ops/delegate_new.js
+++ ops/delegate_new.js
@@ -5,11 +5,12 @@ const keys = require('../helpers/keys.js');
 const constants = require('../helpers/constants.js')
 const transactionFormer = require('../helpers/transactionFormer.js')
 const config = require('../helpers/configReader.js')
+const passArgs = require('../helpers/passArgs.js')
 const popsicle = require('popsicle')
 module.exports=function (vorpal) {
-    return vorpal.command('delegate new <input>').description('Registers new delegate name for current wallet').action(function(args, callback) {
+    return vorpal.command('delegate new <input>').allowUnknownOptions().description('Registers new delegate name for current wallet').action(function(args, callback) {
     	var delegate_name = args.input
-		var keypair = keys.createKeypairFromPassPhrase(config.getConfig().passPhrase)
+		var keypair = keys.createKeypairFromPassPhrase(passArgs.getPassPhrase(args))
     	var data = {type: constants.transactionTypes.DELEGATE, keyPair: keypair, username: delegate_name}
     	var transaction = transactionFormer.createTransaction(data.type, data);
     	var self = this
